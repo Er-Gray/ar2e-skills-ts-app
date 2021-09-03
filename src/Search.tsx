9ebafca22c89832efb030b74e54229150a3a492a
Er-Gray <sakuya.mybride@gmail.com>
2021-09-03 16:57:35 +0900

    絞り込み機能実装の準備

--- src/Search.tsx
+++ src/Search.tsx
@@ -14,7 +14,7 @@ function Search(props:any):React.ReactElement{
 		useRestraint:string;
 		learnRestraint:string;
 		effect:string;
-	}
+	};
 
 	const selectClass=():React.ReactElement[]=>{
 		return Object.values(props.skillData).map((data:skill|any):React.ReactElement=><option key={data.name} value={data.name}>{data.name}</option>);
@@ -22,14 +22,33 @@ function Search(props:any):React.ReactElement{
 
 	const handleClassChange=(event: React.ChangeEvent<HTMLSelectElement>):void=>{
 		props.setViewingClass(event.target.value);
+	};
+
+	const SearchSkillDetail=(name:string):React.ReactElement[]=>{
+		const array:string[]=props.skillData[props.viewingClass].skills.map((data:any):string=>data[name]);
+		const res=new Set(array);
+		const settedArray:string[]=Array.from(res);
+		return settedArray.map((value:string):React.ReactElement=><option key={value} value={value}>{value}</option>);
+	};
+
+	const SearchClass:React.VFC=():React.ReactElement=>{
+		return (
+			<div>
+				<h2>クラス</h2>
+				<select className="selectClass" name="selectClass" defaultValue={props.viewingClass} onChange={handleClassChange}>
+					{selectClass()}
+				</select>
+			</div>
+		);
+	};
+
+	const SearchSkillKinds=(props:any)=>{
+		
 	}
 
 	return (
 		<div>
-			<h2>クラス</h2>
-			<select className="selectClass" name="selectClass" defaultValue={props.viewingClass} onChange={handleClassChange}>
-				{selectClass()}
-			</select>
+			<SearchClass/>
 		</div>
 	);
 }
